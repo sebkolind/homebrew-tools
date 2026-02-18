2c6bd3cfde5cda9928f8809ea6487b756b6ddc82
sebkolind <sks1993@gmail.com>
2026-02-18 13:35:56 +0100

    Add Cistern cask

---
+++ Casks/cistern.rb
@@ -0,0 +1,21 @@
+mkdir -p Casks
+cat > Casks/cistern.rb << 'EOF'
+cask "cistern" do
+  version "1.5"
+  sha256 "ebd16bea020583cb5d680df5f92535c5e5e4a0f7937c04d34e2bcc241bcbbf68"
+
+  url "https://github.com/atombender/cistern/releases/download/#{version}/cistern-release-#{version}.zip"
+  name "Cistern"
+  desc "CircleCI status app for macOS menu bar"
+  homepage "https://github.com/atombender/cistern"
+
+  depends_on macos: ">= :ventura"
+
+  app "Cistern.app"
+
+  zap trash: [
+    "~/Library/Preferences/dev.atombender.Cistern.plist",
+    "~/Library/Logs/Cistern",
+  ]
+end
+EOF
